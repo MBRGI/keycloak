c7062270ed0d02fb85ad5d6288ce96450e866dcf
Loïc Tosser <loic@kalvad.com>
2021-10-24 15:27:15 +0400

    Fix arabic deployment

--- build-arabic.sh
+++ build-arabic.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -e
+set -x
 
 [ -z "$APP_HOME" ] && export APP_HOME=$(pwd)
 [ -z "$KEYCLOAK_VERSION" ] && export KEYCLOAK_VERSION="15.0.2"
 
-#https://github.com/keycloak/keycloak/releases/download/15.0.2/keycloak-15.0.2.zip
-if [ ! -f keycloak-$KEYCLOAK_VERSION.zip ]; then
-    echo "Keycloak not found, Downloading Keycloak version $KEYCLOAK_VERSION"
-    wget -q "https://github.com/keycloak/keycloak/releases/download/$KEYCLOAK_VERSION/keycloak-$KEYCLOAK_VERSION.zip"
-fi
 rm -rf $APP_HOME/keycloak-$KEYCLOAK_VERSION-git
-git clone https://github.com/KalvadTech/keycloak.git -b arabic keycloak-$KEYCLOAK_VERSION-git
+git clone https://github.com/KalvadTech/keycloak.git --depth 1 -b arabic keycloak-$KEYCLOAK_VERSION-git
 cd keycloak-$KEYCLOAK_VERSION-git
 mvn -Pdistribution \
           -pl distribution/server-dist \
           -am \
           -Dmaven.test.skip \
           clean install
+cp distribution/server-dist/target/keycloak-16.0.0-SNAPSHOT $APP_HOME/keycloak-$KEYCLOAK_VERSION
+cd ..
 
 cd config/postgres/main
 rm -f postgresql-42.2.23.jar
